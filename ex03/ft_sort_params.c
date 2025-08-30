9e18129c4ac98c7e9d1c53fecf07c54927c8474a
betty <ibtissambassoma84@gmail.com>
2025-08-30 20:44:52 +0100

    fix bubble sort

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -27,30 +27,30 @@ int	ft_strcmp(char *s1, char *s2)
 void	ft_sort_args(int argc, char **argv)
 {
 	int		i;
-	char	*s1;
-	char	*s2;
 	char	*tmp;
+	int		j;
 
 	i = 1;
-	while (i < argc)
+	while (i < argc - 1)
 	{
-		s1 = argv[i];
-		s2 = argv[i + 1];
-		if (s2 == 0)
-			return ;
-		if (ft_strcmp(s1, s2) > 0)
+		j = 1;
+		while (j < argc - i)
 		{
-			tmp = argv[i];
-			argv[i] = argv[i + 1];
-			argv[i + 1] = tmp;
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				tmp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = tmp;
+			}
+			j++;
 		}
 		i++;
 	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int i;
+	int	i;
 
 	if (argc < 2)
 		return (0);
